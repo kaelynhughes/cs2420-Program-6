4d6d692aa6d160c26e580074bff75799f4e95bcf
Kaelyn Hughes <kaelynmhughes@gmail.com>
2022-11-13 16:32:29 -0700

    Union find complete

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -5,7 +5,7 @@ public class UnionFind {
     public void union(int a, int b) {
         int rootA = find(a);
         int rootB = find(b);
-        // make sure roots arent the same
+        // make sure roots aren't the same
         if (rootA == rootB) {
             System.out.println(a + " and " + b + " are already linked");
             return;
@@ -14,22 +14,20 @@ public class UnionFind {
         if (array[rootA] < array[rootB]) {
             array[rootB] = rootA;
         }
-        // otherwise rootB will have the greater root
+        // if not rootB will either have the greater height or an equal height to rootA
         else {
             array[rootB] = Math.min(array[rootA] - 1, array[rootB]);
             array[rootA] = rootB;
         }
-
     }
 
     public int find(int a) {
-        int parent = array[a];
-        int child = a;
-        while (parent >= 0) {
-            child = parent;
-            parent = array[parent];
+        if (array[a] < 0) {
+            return a;
         }
-        return child;
+        int root = find(array[a]);
+        array[a] = root;
+        return root;
     }
 
     public String toStringAlpha() {
@@ -54,14 +52,19 @@ public class UnionFind {
         for (int i = 0; i < array.length; i++) {
             sb.append(i);
             sb.append(" -> ");
-            if (i == array[i]) {
-                sb.append("**");
+            if (array[i] < 0) {
+                sb.append("root (height ");
+                sb.append(-1 * array[i]);
+                sb.append(")");
             } else {
                 sb.append(array[i]);
             }
             sb.append("\n");
         }
         return sb.toString();
+    }
+    public int getSize() {
+        return array.length;
     }
 
     public UnionFind() {
@@ -75,7 +78,15 @@ public class UnionFind {
         }
     }
 
+    public UnionFind(int[] provided) {
+        array = new int[provided.length];
+        for (int i = 0; i < provided.length; i++) {
+            array[i] = provided[i];
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("Testing union on unlinked & linked nodes: ");
         UnionFind set1 = new UnionFind('k' + 1);
         set1.union('d', 'a');
         set1.union('e', 'b');
@@ -85,12 +96,31 @@ public class UnionFind {
         set1.union('i', 'h');
         set1.union('e', 'a');
         System.out.println(set1.toStringAlpha());
+        System.out.println("Performing find on f and e:");
         System.out.println((char) set1.find('f'));
         System.out.println((char) set1.find('e'));
         System.out.println();
+        System.out.println("After find:");
         System.out.println(set1.toStringAlpha());
 
+        System.out.println("Testing default constructor & performing many unions:");
         UnionFind set2 = new UnionFind();
+        for (int i = set2.getSize() - 3; i > 10; i -= 2) {
+            set2.union(i, i + 1);
+        }
+        for (int i = set2.getSize() - 4; i > 10; i -= 2) {
+            set2.union(i, i + 1);
+        }
+        set2.union(119, 120);
+        System.out.println(set2);
 
+        System.out.println("Testing path compression\n");
+        int[] array = {2, 0, -4, 0, 1, 2, -1, 2, 1};
+        UnionFind set3 = new UnionFind(array);
+        System.out.println("Before find on 4:");
+        System.out.println(set3);
+        set3.find(4);
+        System.out.println("After find on 4:");
+        System.out.println(set3);
     }
 }
